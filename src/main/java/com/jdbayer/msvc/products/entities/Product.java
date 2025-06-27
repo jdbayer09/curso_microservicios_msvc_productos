6f691be946a43c6cdb4cb736cc4f47ea35fd7153
Julian David Bayer <jdbayer09@hotmail.com>
2025-06-27 16:47:52 -0500

    Configuracion de entidad

---
+++ src/main/java/com/jdbayer/msvc/products/entities/Product.java
@@ -0,0 +1,39 @@
+package com.jdbayer.msvc.products.entities;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+public class Product implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -8348180920828187573L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(length = 100, nullable = false, name = "name")
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
+
+    @Column(name = "create_at", nullable = false)
+    private ZonedDateTime createAt;
+}
